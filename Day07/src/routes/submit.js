825245f5edda14462deb848cf35d34b9dbc0f70e
Shiva <148767570+Sh1va84@users.noreply.github.com>
2026-05-16 19:19:38 +0530

    Delete Day07 directory

--- Day07/src/routes/submit.js
+++
@@ -1,7 +0,0 @@
-
-const express = require('express');
-const submitRouter = express.Router();
-const userMiddleware = require("../middleware/userMiddleware");
-const submitCode = require("../controllers/userSubmission");
-
-submitRouter.post("/submit/:id", userMiddleware, submitCode);
